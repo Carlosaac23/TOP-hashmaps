a7603aacd9aafdbf32744f96cd49fd1d452a13e6
Carlos <carlosaac232001@gmail.com>
2025-10-23 15:36:30 -0500

    feat: - update que HashMap class with the constant numbers of load_factor and capacity - create the first methos which is hash that recieve a string and return a hash value

--- hash-map.js
+++ hash-map.js
@@ -1,6 +1,18 @@
 export class HashMap {
-  constructor(load_factor, capacity) {
+  constructor(load_factor = 0.75, capacity = 16) {
     this.load_factor = load_factor;
     this.capacity = capacity;
+    this.buckets = new Array(capacity);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
   }
 }
